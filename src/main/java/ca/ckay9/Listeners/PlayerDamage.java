c9f642935693991a26d8f0c9af2f682494a0873a
Cameron A. <foxtrotcyanide@gmail.com>
2025-06-06 10:08:30 -0700

    Combat logs now effect damager.

--- src/main/java/ca/ckay9/Listeners/PlayerDamage.java
+++ src/main/java/ca/ckay9/Listeners/PlayerDamage.java
@@ -1,13 +1,18 @@
 package ca.ckay9.Listeners;
 
+import java.util.HashMap;
+import java.util.UUID;
+
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 
 import ca.ckay9.CxWar;
 import ca.ckay9.Storage;
+import ca.ckay9.Utils;
 
 public class PlayerDamage implements Listener {
     private CxWar cx_war;
@@ -17,12 +22,31 @@ public class PlayerDamage implements Listener {
     }
 
     @EventHandler(priority = EventPriority.LOWEST)
-    public void onPlayerDamage(EntityDamageEvent event) {
+    public void onPlayerDamage(EntityDamageByEntityEvent event) {
         if (!(event.getEntity() instanceof Player)) {
             return;
         }
 
+        int log_time = Storage.config.getInt("tp.combat_log_time", 300);
+        HashMap<UUID, Integer> logs = this.cx_war.teleports.combat_logs;
+
         Player player = (Player)event.getEntity();
-        cx_war.teleports.combat_logs.put(player.getUniqueId(), Storage.config.getInt("tp.combat_log_time", 300));
+        UUID player_uuid = player.getUniqueId();
+        if (logs.get(player_uuid) == null || logs.get(player_uuid) == 0) {
+            player.sendMessage(Utils.formatText("&cYou have been combat logged for &c&l" + log_time + "s"));
+        }
+
+        cx_war.teleports.combat_logs.put(player_uuid, log_time);
+        if (!(event.getDamager() instanceof Player)) {
+            return;
+        }
+
+        Player damager = (Player)event.getDamager();
+        UUID damager_uuid = damager.getUniqueId();
+        if (logs.get(damager_uuid) == null || logs.get(damager_uuid) == 0) {
+            player.sendMessage(Utils.formatText("&cYou have been combat logged for &c&l" + log_time + "s"));
+        }
+
+        cx_war.teleports.combat_logs.put(damager_uuid, log_time);
     }
 }
